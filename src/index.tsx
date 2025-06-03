21332a889f78f9baa10cf8659a674f91c080dc3e
okayus <toshiaki.mukai.9981@gmail.com>
2025-06-03 22:00:01 +0900

    init

---
+++ src/index.tsx
@@ -0,0 +1,35 @@
+import { KintoneRestAPIClient } from "@kintone/rest-api-client";
+
+import { renderExecutionButton } from "./components/desktopUIHelpers";
+import { MessageService } from "./service/MessageService";
+import { KintoneSdk } from "./util/kintoneSdk";
+
+import type { Record } from "@kintone/rest-api-client/lib/src/client/types";
+import type { KintoneEvent } from "src/types/KintoneTypes";
+
+// メイン処理
+(() => {
+  kintone.events.on("app.record.index.show", async (event: KintoneEvent) => {
+    const restApiClient = new KintoneRestAPIClient();
+    const kintoneSdk = new KintoneSdk(restApiClient);
+    const messageService = new MessageService(kintoneSdk);
+
+    const handleAlertButtonClick = async () => {
+      try {
+        const records = await messageService.fetchRecords(event.appId);
+        if (records.length > 0) {
+          messageService.alertMessage(records as Record[]);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        throw error;
+      }
+    };
+
+    renderExecutionButton(
+      "alert-button",
+      handleAlertButtonClick,
+      "メッセージを表示",
+    );
+  });
+})();
